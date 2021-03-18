73949f945ab1f6fab7c2cac92a0b5d3cfa42c1d9
Alfredo <thealf154@gmail.com>
2021-03-18 14:15:08 -0600

    Completar Objetos

--- src/Marinos/Pez.java
+++ src/Marinos/Pez.java
@@ -1,19 +1,37 @@
 package Marinos;
 
 public class Pez extends Marinos {
-    Pez(String sistemaDigestivo, String tiempoDeVida, boolean tieneAletas, Double temperatura, Double presión) {
+    private String tipoDePez;
+    private boolean esDeAguaSalada;
+
+    Pez(String sistemaDigestivo, String tiempoDeVida, boolean tieneAletas, Double temperatura, Double presión,
+            String tipoDePez, boolean esDeAguaSalada) {
         super(sistemaDigestivo, tiempoDeVida, tieneAletas, temperatura, presión);
     }
 
-    private void beberDelRío(){
+    protected void setTipoDePez(String tipo){
+        this.tipoDePez = tipo;
+    }
+
+    protected String getTipoDePez(){
+       return this.tipoDePez; 
+    }
+
+    protected void setEsDeAguaSalada(boolean aguaSalada){
+        this.esDeAguaSalada = aguaSalada;
+    }
+
+    protected boolean getEsDeAguaSalada(){
+        return this.esDeAguaSalada;
+    }
+
+    protected void beberDelRío() {
         System.out.println("El pez esta bebiendo del río");
     }
 
-    public static void main(String[] args) {
-       Pez Pez = new Pez("Herbivoro", "10 años", true, 70.0, 70.0);
-       Pez.Nadar();
-       Pez.reproducirse();
-       Pez.beberDelRío();
-       System.out.println(Pez.getTieneAletas());
-    }
+    /*
+     * public static void main(String[] args) { Pez Pez = new Pez("Herbivoro",
+     * "10 años", true, 70.0, 70.0); Pez.Nadar(); Pez.reproducirse();
+     * Pez.beberDelRío(); System.out.println(Pez.getTieneAletas()); }
+     */
 }
--- src/Marinos/Tiburón.java
+++ src/Marinos/Tiburón.java
@@ -1,11 +1,32 @@
 package Marinos;
 
-public class Tiburón extends Marinos{
-    Tiburón(String sistemaDigestivo, String tiempoDeVida, boolean tieneAletas, Double temperatura, Double presión) {
+public class Tiburón extends Marinos {
+    private boolean haComidoHumanos;
+    private boolean esAgresivo;
+
+    Tiburón(String sistemaDigestivo, String tiempoDeVida, boolean tieneAletas, Double temperatura, Double presión,
+            boolean haComidoHumanos, boolean esAgresivo) {
         super(sistemaDigestivo, tiempoDeVida, tieneAletas, temperatura, presión);
+
     }
 
-    protected void jaws(){
+    protected void setHaComidoHumanos(boolean comer){
+        this.haComidoHumanos = comer;
+    }
+
+    protected boolean getHaComidoHumanos(){
+        return this.haComidoHumanos;
+    }
+
+    protected void setEsAgresivo(boolean agresivo){
+        this.esAgresivo = agresivo;
+    }
+
+    protected boolean getEsAgresivo(){
+        return this.esAgresivo;
+    }
+
+    protected void jaws() {
         System.out.println("Dun dun dun dundun");
     }
 }
