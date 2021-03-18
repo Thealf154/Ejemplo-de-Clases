90e727fe999fc75ac4be37e7a1e30e76bffd8204
Alfredo <thealf154@gmail.com>
2021-03-18 14:57:46 -0600

    Cambios a la ballena

--- src/Marinos/Ballena.java
+++ src/Marinos/Ballena.java
@@ -1,7 +1,24 @@
 package Marinos;
 
-public class Ballena extends Marinos{
-    Ballena(String sistemaDigestivo, String tiempoDeVida, boolean tieneAletas, Double temperatura, Double presión) {
+public class Ballena extends Marinos {
+    private long cantidadDePlanktonComido;
+
+    Ballena(String sistemaDigestivo, String tiempoDeVida, boolean tieneAletas, Double temperatura, Double presión,
+            long cantidadDePlanktonComido) {
         super(sistemaDigestivo, tiempoDeVida, tieneAletas, temperatura, presión);
+        this.cantidadDePlanktonComido = cantidadDePlanktonComido;
+    }
+
+    protected void setCantidadDePlanktonComido(long plankton) {
+        this.cantidadDePlanktonComido = plankton;
+    }
+
+    protected long getCantidadDePlanktonComido() {
+        return this.cantidadDePlanktonComido;
+    }
+
+    protected void comePlankton(){
+        this.cantidadDePlanktonComido += 1000000;
+        System.out.println("Comí 1000000 de plankton");
     }
 }
