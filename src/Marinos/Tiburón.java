50ca90c56c54a3dfda4d4f1e3858cf461759a782
Alfredo <thealf154@gmail.com>
2021-03-18 13:57:50 -0600

    Añadi función

--- src/Marinos/Tiburón.java
+++ src/Marinos/Tiburón.java
@@ -4,4 +4,8 @@ public class Tiburón extends Marinos{
     Tiburón(String sistemaDigestivo, String tiempoDeVida, boolean tieneAletas, Double temperatura, Double presión) {
         super(sistemaDigestivo, tiempoDeVida, tieneAletas, temperatura, presión);
     }
+
+    protected void jaws(){
+        System.out.println("Dun dun dun dundun");
+    }
 }
