2d2acb13b64d8add3baf8bf644162d00a826a581
Alfredo <thealf154@gmail.com>
2021-03-18 14:53:06 -0600

    Refinar funciones

--- src/Marinos/Delfín.java
+++ src/Marinos/Delfín.java
@@ -1,7 +1,47 @@
 package Marinos;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Delfín extends Marinos {
-    Delfín(String sistemaDigestivo, String tiempoDeVida, boolean tieneAletas, Double temperatura, Double presión) {
+    private boolean sabeTrucos;
+    private boolean obedeceOrdenes;
+
+    Delfín(String sistemaDigestivo, String tiempoDeVida, boolean tieneAletas, Double temperatura, Double presión,
+            boolean sabeTrucos, boolean obedeceOrdenes) {
         super(sistemaDigestivo, tiempoDeVida, tieneAletas, temperatura, presión);
+        this.sabeTrucos = sabeTrucos;
+        this.obedeceOrdenes = obedeceOrdenes;
+    }
+
+    protected void hazTruco() {
+        try (BufferedReader br = new BufferedReader(new FileReader("src/Marinos/Delfin.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException f) {
+            System.out.println("No encontrado");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void setSabeTrucos(boolean sabeTrucos) {
+        this.sabeTrucos = sabeTrucos;
+    }
+
+    protected boolean getSabeTrucos() {
+        return this.sabeTrucos;
+    }
+
+    protected void setObedeceOrdenes(boolean obedeceOrdenes) {
+        this.obedeceOrdenes = obedeceOrdenes;
+    }
+
+    protected boolean getObedeceOrdenes() {
+        return this.obedeceOrdenes;
     }
 }
