f1e67cedf1a5bf7668f069a964565a890abe890a
Raulh229 <raulix.xd@gmail.com>
2021-03-23 16:52:36 -0600

    Aves

--- src/Aves/Gallina.java
+++ src/Aves/Gallina.java
@@ -5,18 +5,9 @@ public class Gallina extends Aves{
     private String Colordepluma;
     private boolean Planea; 
     
-    Gallina(String sistemaDigestivo, String tiempoDeVida,String tipodevuelo, boolean vuela, String colordepluma, boolean planea) {
-        super(sistemaDigestivo, tiempoDeVida, tipodevuelo, vuela);
-        this.Colordepluma = colordepluma;
+    Gallina(String sistemaDigestivo, String tiempoDeVida,String tipodevuelo, boolean vuela, String plumaje, boolean planea) {
+        super(sistemaDigestivo, tiempoDeVida, tipodevuelo, vuela, plumaje);
         this.Planea = planea;
-    }
-
-    public void setColordepluma(String colordepluma){
-        this.Colordepluma = colordepluma;
-    }
-    
-    public String getColordepluma(){
-        return this.Colordepluma;
     }
 
     public void setPlanea(boolean planea){
@@ -40,7 +31,7 @@ public class Gallina extends Aves{
     public static void main(String[] args) {
        Gallina gallina1 = new Gallina("Herbivoro", "1 año", "Por momentos", false, "cafe", true);
        
-       System.out.println("La gallina tiene plumas color " + gallina1.getColordepluma());
+       System.out.println("La gallina es color " + gallina1.getPlumaje());
        
        gallina1.volar();
        gallina1.planear();
