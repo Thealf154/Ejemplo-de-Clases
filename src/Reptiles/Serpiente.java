5862d2b2775761f956a5b9008a8df3e6458e9e8f
edu0603 <80263363+edu0603@users.noreply.github.com>
2021-03-24 10:57:16 -0600

    Update Serpiente.java
    cambios serpientes

--- src/Reptiles/Serpiente.java
+++ src/Reptiles/Serpiente.java
@@ -1,30 +1,39 @@
 package Reptiles;
 
-public class Serpiente extends Reptiles {
-	private boolean tieneCola;
-	private String textura; 
 
-	Serpiente(String sistemaDigestivo, String tiempoDeVida, boolean esOviparo, Double temperatura, boolean exoesqueleto,
-			String textura, boolean tienecola) {
-		super(sistemaDigestivo, tiempoDeVida, esOviparo, temperatura, exoesqueleto, textura);
-	}
+	public class Serpiente extends Reptiles {
+	    Serpiente(String sistemaDigestivo, String tiempoDeVida, Double TemperaturaCorporal,  boolean tienecola) {
+	        super(sistemaDigestivo, tiempoDeVida);
+	        this.arrastrarse();
+	        this.viviparo();
+	    }
+	    
+	
+	    
+	
+	    protected void arrastrarse(){
+	        System.out.println("La serpiente se esta arrastrando");
+	        this.arrastrarse();
+	    }
+	    
+	    
+		protected void viviparo() {
+			
+			System.out.println("la serpiente es vivipara");
+			this.viviparo();
+	    }	
+			
+		
+	    
+
+		public static void main(String[] args) {
+	       Serpiente serpiente = new Serpiente("carnivoro", "25 años" , 38.0, true);
+	       serpiente.arrastrarse();
+	       serpiente.reproducirse();
+	       serpiente.respirar();
+	       serpiente.viviparo();
+	    }
 
 
-	public void arrastrarse() {
-		System.out.println("La serpiente se esta arrastrando");
-	}
-
-	//Debe ir camelCase en caso de que tenga más de una palabra
-	private void esViviparo() {
-		System.out.println("la serpiente es vivipara");
-	}
-
-	//Tienes que construir un mejor main
-	/*public static void main(String[] args) {
-		Serpiente serpiente = new Serpiente("carnivoro", "25 años", 38.0, true,);
-		serpiente.arrastrarse();
-		serpiente.reproducirse();
-		serpiente.respirar();
-		serpiente.viviparo();
-	}*/
-}
+	    
+		}
