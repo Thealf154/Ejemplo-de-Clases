7c8086c95b5f81c7407311d4b1dd92327a707d95
edu0603 <80263363+edu0603@users.noreply.github.com>
2021-03-24 10:55:56 -0600

    Update Cocodrile.java
    cambios en cocodrile

--- src/Reptiles/Cocodrile.java
+++ src/Reptiles/Cocodrile.java
@@ -1,26 +1,40 @@
 package Reptiles;
 
-public class Cocodrile extends Reptiles {
-	Cocodrile(String sistemaDigestivo, String tiempoDeVida, boolean esOviparo, Double temperatura, boolean exoesqueleto,
-			String textura) {
-		super(sistemaDigestivo, tiempoDeVida, esOviparo, temperatura, exoesqueleto, textura);
-	}
 
-	protected static void main(String[] args) {
-		Cocodrile cocodrile = new Cocodrile("carnivoro", "70 años", 50.0, true);
-		cocodrile.arrastrarse();
-		cocodrile.reproducirse();
-		cocodrile.respirar();
-		cocodrile.viviparo();
-	}
+	public class Cocodrile extends Reptiles {
+	    Cocodrile(String sistemaDigestivo, String tiempoDeVida, Double TemperaturaCorporal,  boolean tienecola) {
+	        super(sistemaDigestivo, tiempoDeVida);
+	        this.arrastrarse();
+	        this.viviparo();
+	    }
+	    
+	
+	    
+	
 
-	protected void arrastrarse() {
-		System.out.println("El cocodrilo se arrastra");
-	}
+	    
 
-	protected void viviparo() {
+		public static void main(String[] args) {
+	       Cocodrile cocodrile = new Cocodrile("carnivoro", "70 años" , 50.0, true);
+	       cocodrile.arrastrarse();
+	       cocodrile.reproducirse();
+	       cocodrile.respirar();
+	       cocodrile.viviparo();
+	    }
 
-		System.out.println("El cocodrilo es viviparo");
-	}
 
-}
+	    
+	    protected void arrastrarse(){
+	        System.out.println("El cocodrilo se arrastra");
+	    }
+	    
+	    
+		protected void viviparo() {
+			
+			System.out.println("El cocodrilo es viviparo");
+	    }	
+			
+		}
+
+		
+
